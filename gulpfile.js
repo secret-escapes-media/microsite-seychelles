e912d36c1652184dfff4d56f15f42aff8ae85f2c
Hamish Gray <hamish.gray@secretescapes.com>
2018-04-26 15:42:25 +1200

    final changes, purchased images, testing checklist complete

--- gulpfile.js
+++ gulpfile.js
@@ -108,17 +108,12 @@ gulp.task('build-main-js', function(cb) {
 
     // plugins
     './node_modules/jquery/dist/jquery.min.js',
-    './node_modules/jquery-countdown/dist/jquery.countdown.min.js',
-    './node_modules/waypoints/lib/jquery.waypoints.min.js',
 
     // custom js - with on doc ready wrapper
     './_assets/js/_components/on-ready/start.js',
 
     // components
     './_assets/js/_components/standard.js',
-    './_assets/js/_components/offer-countdown.js',
-    './_assets/js/_components/modal.js',
-    './_assets/js/_components/modal-nav.js',
     './_assets/js/_components/sticky-nav.js',
 
     // custom js for project
