c5934ceff60bb5d4679fcfcd3365d49a15cc982b
Hamish Gray <hamish.gray@secretescapes.com>
2018-04-27 11:54:17 +1200

    final changes. guide hosted live. strutta integrated. checklist complete

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/seychelles': '_site/'
+        '/it/seychelles': '_site/'
       }
     }
   });
